02811ecf4719d5e5356cd340e3b8b8330e92b0cb
Ariane Barboza <arianeb@ciandt.com>
2022-11-29 16:51:15 -0300

    ""Ariane01-método-sort"

---
+++ Java8/src/OrdenaStrings.java
@@ -0,0 +1,36 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenaStrings {
+
+    public static void main(String[]args){
+
+        List<String> palavras = new ArrayList<String>();
+        palavras.add("alura online");
+        palavras.add("editora cada do código");
+        palavras.add("caelum");
+
+        //sai na ordem alfabética - LEXCOGRÁFICA DO A até Z
+        Collections.sort(palavras);
+        System.out.println(palavras);
+
+        //Ao rodar o resultado sai: [alura online, caelum, editora cada do código],
+        //sai na ordem alfabética - LEXCOGRÁFICA DO A até Z
+
+
+    }
+
+}
+
+class ComparadorPorTamanho implements Comparator<String>{
+
+
+    @Override
+    public int compare(String s1, String s2) {
+        return 0;
+    }
+}
